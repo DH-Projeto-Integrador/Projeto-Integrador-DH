dc9e889c73c9b9227c96c3f60598de7e0a921464
Savio Nascimento <savio.nascimento.gomes@gmail.com>
2022-10-13 15:21:24 -0300

    Controller da tabela vendas

---
+++ src/controllers/VendaController.js
@@ -0,0 +1,97 @@
+const VendaModel = require('../models/VendaModel');
+
+module.exports = {
+
+    async getAll() {
+
+        const [_req, res] = arguments
+
+        const sales = await VendaModel.findAll();
+    
+        return res.json(sales);
+
+    },
+
+    async store() {
+
+        const [req, res] = arguments;
+
+        const { date_sell, amount, status_sale,  } = req.body;
+
+        const sale = await VendaModel.create({ name });
+    
+        if(sale) {
+            return res
+        .status(201)
+        .json(sale);
+        } else {
+            res
+            .status(401)
+            .json({Message: "Venda não cadastrada."})
+        }       
+
+    },
+    async getById() {
+        const [req, res] = arguments;
+
+        const { id } = req.params;
+
+        const saleFound = await VendaModel.findByPk(id);
+
+        if (saleFound) {
+            res
+                .status(200)
+                .json(saleFound);
+        }
+        else {
+            res
+            .status(404)
+            .json({ Message: 'Venda não encontrada' })
+        }
+    },
+    async updateById() {
+
+        const [req, res] = arguments;
+
+        const { id } = req.params;
+
+        const saleFound = await VendaModel.findByPk(id);
+
+        if (saleFound) {
+
+            saleFound.set(req.body);
+
+            const saleUpdated = await saleFound.save();
+
+            res
+                .status(200)
+                .json(saleUpdated);
+        }
+        else {
+            res
+                .status(404)
+                .json({ message: 'Venda não encontrada' })
+        }
+    },
+
+    async deleteById() {
+
+        const [req, res] = arguments;
+
+        const { id } = req.params;
+
+        const saleFound = await VendaModel.findByPk(id);
+
+        if (saleFound) {
+            saleFound.destroy();
+            res
+                .status(200)
+                .json({Message:`Venda deletada com sucesso!`});
+        }
+        else {
+            res
+                .status(404)
+                .json({ message: 'Venda não encontrada' });
+        }
+    }
+}
