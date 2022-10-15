75cc4f7b415015b3ce52513b984f409b114fd663
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-10-14 22:58:25 -0300

    migration da tabela intermediária venda_itens

---
+++ src/database/migrations/20221014125523-create-venda_itens.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+
+    return queryInterface.createTable('venda_itens', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      id_sale: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        refereces: {
+          model: 'vendas',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      id_product: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        refereces: {
+          model: 'produtos',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      qty_products: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price_sold: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+    down: (queryInterface, Sequelize) => {
+
+    return queryInterface.dropTable('venda_itens');
+
+  }
+};
