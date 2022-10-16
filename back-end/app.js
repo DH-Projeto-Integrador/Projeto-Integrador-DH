2c2b589c7edc3f479bf9a21195871ff85bdddade
João Antonio <hiukakamachi@gmail.com>
2022-10-16 10:14:00 -0300

    projeto desestruturado para back e front

---
+++ back-end/app.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+
+require("./src/database");
+
+//Configutações
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join("public")));
+app.use(cors());
+
+// Importação de Rotas
+const cadastroUsuario = require("./src/routers/cadastroUsuario");
+const perfilUsuario = require("./src/routers/perfilUsuario");
+const home = require("./src/routers/home");
+const produto = require("./src/routers/produto");
+const login = require("./src/routers/login");
+const recuperarSenha = require("./src/routers/recuperarSenha");
+const sobre = require("./src/routers/sobre");
+const carrinho = require("./src/routers/carrinho");
+const routesDb = require("./src/routers/routesDb");
+
+// Rotas
+app.use("/", cadastroUsuario);
+app.use("/", perfilUsuario);
+app.use("/", home);
+app.use("/", produto);
+app.use("/", login);
+app.use("/", recuperarSenha);
+app.use("/", sobre);
+app.use("/", carrinho);
+app.use("/", routesDb);
+
+// Dev
+app.get("/dev/header", (req, res) =>
+  res.sendFile(path.join(__dirname, "/src/views/header.dev.html"))
+);
+
+app.listen(process.env.SERVER_PORT, console.log(`Servidor rodando`));
---
+++ back-end/src/config/database.js
@@ -0,0 +1,15 @@
+//Importação do dotenv para utilização das variáveis de ambiente
+require("dotenv").config();
+
+//Configurações do banco de dados
+module.exports = {
+  dialect: "mysql",
+  host: process.env.SERVER_HOST,
+  username: process.env.SERVER_USER,
+  password: process.env.SERVER_PASSWORD,
+  database: process.env.SERVER_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+  },
+};
