d2ffa5f36eade05bc77917c7794dd1deb0808ac7
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-10-28 15:23:48 -0300

    ajuste no tamaho e na rederização dos cartões

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -2,50 +2,40 @@ import { useNavigate } from "react-router-dom";
 
 import { Link } from "react-router-dom";
 
-import { Button } from "./Button"
-
-export function Card({ product, url, price, name, ...props }) {
+export function Card({ product }) {
 
   const navigate = useNavigate();
   
   function GoToProduct() {
-    
     navigate(`/${product.id}`);
 
   }
 
   return (
 
-    <Link
-      {...props}
-      className="block w-full max-w-sm h-max bg-white rounded-lg border border-gray-200 shadow-md"
-      onClick={ () => {GoToProduct()} }
+    <Link to={`/${product.id}`}
+      className="grid grid-rows-6 w-full max-w-[20rem] h-72 bg-white rounded-lg border border-gray-200 shadow-md"
     >
-      <div className="h-3/4">
-        <div>
+        <div className="row-span-4">
           <img
-            className="rounded-t-lg"
+            className="w-full h-full rounded-t-lg"
             // src={url}
             src={product.image_product}
-            alt={product.name}
+            alt={product.description}
           />
         </div>
-        <div className="p-5">
+
+        <div className="p-5 row-span-2 h-max">
           <span>
-            {/* <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {name}
-            </h5> */}
             <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
               {product.name}
             </h5>
           </span>
-          {/* <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            {price}
-          </span> */}
+          
           <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             {product.price}
           </span>
-      </div>
+
       </div>
     </Link>
   );
