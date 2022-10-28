9cb3355d706e5485138c5074903ee5490cef2440
João Antonio <hiukakamachi@gmail.com>
2022-10-28 19:18:45 -0300

    adiconando token jwt pelo lado do servidor

--- back-end/src/controllers/UsuarioController.js
+++ back-end/src/controllers/UsuarioController.js
@@ -1,8 +1,9 @@
 const UsuarioModel = require("../models/UsuarioModel");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   async getAll() {
-    const [_req, res] = arguments;
+    const [, res] = arguments;
 
     const users = await UsuarioModel.findAll();
 
@@ -25,12 +26,28 @@ module.exports = {
       full_address,
     });
 
-    if (user) {
-      return res.status(201).json(user);
-    } else {
-      res.status(401).json({ Message: "Usuario não cadastrado." });
+    if (!user) {
+      res.status(401).json({ Message: "Erro ao cadastrar usuario" });
     }
+
+    const token = jwt.sign(
+      {
+        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        data: {
+          id: user.id,
+          name: user.name,
+          lastname: user.lastname,
+          email: user.email,
+          cpf: user.cpf,
+          full_address: user.full_address,
+        },
+      },
+      process.env.SECRET_TOKEN
+    );
+
+    return res.status(201).json(token);
   },
+
   async getById() {
     const [req, res] = arguments;
 
@@ -38,12 +55,42 @@ module.exports = {
 
     const userFound = await UsuarioModel.findByPk(id);
 
-    if (userFound) {
-      res.status(200).json(userFound);
-    } else {
+    if (!userFound) {
       res.status(404).json({ Message: "Usuario não encontrado" });
     }
+
+    return res.status(200).json(userFound);
   },
+
+  async getByEmail() {
+    const [req, res] = arguments;
+
+    const { email, password } = req.body;
+
+    const userFound = await UsuarioModel.findOne({where: { email }});
+
+    if (userFound && userFound.password == password ) {
+      const token = jwt.sign(
+        {
+          exp: Math.floor(Date.now() / 1000) + 60 * 60,
+          data: {
+            id: userFound.id,
+            name: userFound.name,
+            lastname: userFound.lastname,
+            email: userFound.email,
+            cpf: userFound.cpf,
+            full_address: userFound.full_address,
+          },
+        },
+        process.env.SECRET_TOKEN
+      );
+
+      return res.status(200).json(token);
+    }
+    
+    return res.status(404).json({ Message: "Login invalido" });
+  },
+
   async updateById() {
     const [req, res] = arguments;
 
@@ -51,15 +98,15 @@ module.exports = {
 
     const userFound = await UsuarioModel.findByPk(id);
 
-    if (userFound) {
-      userFound.set(req.body);
-
-      const userUpdated = await userFound.save();
-
-      res.status(200).json(userUpdated);
-    } else {
-      res.status(404).json({ message: "Usuario não encontrado" });
+    if (!userFound) {
+      return res.status(404).json({ message: "Usuario não encontrado" });
     }
+
+    userFound.set(req.body);
+
+    const userUpdated = await userFound.save();
+
+    return res.status(200).json(userUpdated);
   },
 
   async deleteById() {
@@ -69,11 +116,11 @@ module.exports = {
 
     const userFound = await UsuarioModel.findByPk(id);
 
-    if (userFound) {
-      userFound.destroy();
-      res.status(200).json({ Message: `Usuario deletada com sucesso!` });
-    } else {
+    if (!userFound) {
       res.status(404).json({ message: "Usuario não encontrado" });
     }
+
+    userFound.destroy();
+    res.status(200).json({ Message: `Usuario deletada com sucesso!` });
   },
 };
