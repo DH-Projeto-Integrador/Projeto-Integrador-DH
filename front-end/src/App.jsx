1e7f68eb21d3a5595a2ec1a638484b303d749097
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-10-27 22:30:32 -0300

    ajustando o app

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,17 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import { CardProvider } from "./providers/card.provider";
 
 import "./styles/global.css";
 
 export function App() {
   return (
     <>
-      <Header />
-      
-      <Outlet />
-
-      <Footer />
+      <CardProvider>
+        <Header />
+        <Outlet />
+        <Footer />
+      </CardProvider>
     </>
   );
 }
