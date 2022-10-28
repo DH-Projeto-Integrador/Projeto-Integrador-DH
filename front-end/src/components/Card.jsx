e4d514212dbd4f697f8f49fb47636de0f8e66348
Isac Souza Filho <souzafilhoisac@gmail.com>
2022-10-27 22:20:14 -0300

    iniciando ajustes no cartão

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -1,37 +1,55 @@
+import { useNavigate } from "react-router-dom";
+
 import { Link } from "react-router-dom";
 
-export function Card({ url, price, name, ...props }) {
+export function Card({ product, url, price, name, ...props }) {
+
+  const navigate = useNavigate();
+
+  function GoToProduct() {
+
+    navigate(`/${product.id}`);
+
+  }
+
   return (
+
     <Link
       {...props}
       className="block w-full max-w-sm h-max bg-white rounded-lg border border-gray-200 shadow-md"
     >
-      <div>
-        <img
-          className="rounded-t-lg"
-          // src={url}
-          src="https://media.istockphoto.com/photos/strawberries-ripe-red-fresh-background-in-a-local-super-market-picture-id1209321568?b=1&k=20&m=1209321568&s=170667a&w=0&h=grgax5nRWCvVtaCIgbDKf3joueQnezHw3ATNk5jWefI="
-          alt=""
-        />
+      <div className="h-3/4">
+        <div>
+          <img
+            className="rounded-t-lg"
+            // src={url}
+            src={product.image_product}
+            alt={product.name}
+          />
+        </div>
+        <div className="p-5">
+          <span>
+            {/* <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {name}
+            </h5> */}
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {product.name}
+            </h5>
+          </span>
+          {/* <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {price}
+          </span> */}
+          <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            {product.price}
+          </span>
+          <button 
+          type="button"
+          className="w-full text-white rounded-2xl hover:bg-red-700 py-2 px-4 bg-red-500 text-center" 
+          onClick={ () => {GoToProduct() }} 
+          >
+            Detalhes
+          </button>
       </div>
-
-      <div className="p-5">
-        <span>
-          {/* <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {name}
-          </h5> */}
-          <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Morango
-          </h5>
-        </span>
-
-        {/* <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {price}
-        </span> */}
-
-        <span className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          $ 12,00
-        </span>
       </div>
     </Link>
   );
